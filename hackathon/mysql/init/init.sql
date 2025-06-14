40fdd7cc19907c1086aa24ed78553b97289e952c
Towa-Fujiwara <t-fujiwara@g.ecc.u-tokyo.ac.jp>
2025-06-14 17:39:00 +0900

    aaa

--- hackathon/mysql/init/init.sql
+++ hackathon/mysql/init/init.sql
@@ -1,11 +1,8 @@
 CREATE TABLE users (
                        id VARCHAR(50) PRIMARY KEY,
                        name VARCHAR(50) NOT NULL UNIQUE,
-                       password TEXT NOT NULL,
-                       display_name TEXT NOT NULL,
                        bio TEXT,
                        icon_url TEXT,
-                       age INTEGER,
                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
