72a6cd89679c606798d84748b6bc09fe26753a21
Towa-Fujiwara <t-fujiwara@g.ecc.u-tokyo.ac.jp>
2025-06-15 00:02:57 +0900

    a

--- hackathon/controller/cors_middleware.go
+++ hackathon/controller/cors_middleware.go
@@ -13,7 +13,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
             "http://localhost:5173",
             "http://localhost:3000",
 			"https://hackathon-frontend-one-khaki.vercel.app",
-			"https://hackathon-frontend-jp4adbcio-towa-fujiwaras-projects.vercel.app",
+			"https://hackathon-frontend-o0ty9vtbr-towa-fujiwaras-projects.vercel.app",
         }
         
         origin := r.Header.Get("Origin")
